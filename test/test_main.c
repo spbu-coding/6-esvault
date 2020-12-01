19b00d65edf66a31b9c850b1787b500280fa4034
esvault <54893829+esvault@users.noreply.github.com>
2020-12-01 23:24:46 +0300

    Update test_main.c

--- test/test_main.c
+++ test/test_main.c
@@ -24,7 +24,7 @@ size_t DEFAULT_ARGS_SIZE = 6;
 // Assumes that there are <= 10 string and each size is <= 1000
 // Hope no errors will arise :)
 int cmp_file_and_str(const char* expected) {
-    printf("27\n");
+    fprintf(stderr, "27\n");
     FILE* f = fopen(OUTPUT_FILENAME, "r");
 
     // use any non 0 as failed status
@@ -34,16 +34,16 @@ int cmp_file_and_str(const char* expected) {
     char all_data[MAX_INPUT_STRING_SIZE * 10] = "";
 
     if (f != NULL) {
-        printf("37\n");
+        fprintf(stderr, "37\n");
         while (fgets(str, MAX_INPUT_STRING_SIZE, f) != NULL) {
             strcat(all_data, str);
         }
-        printf("41\n");
+        fprintf(stderr, "41\n");
         // Commented code below can be useful for debug
         fprintf(stderr, "Got: %s\n", all_data);
         fprintf(stderr, "Exp: %s\n", expected);
         result = strcmp(all_data, expected);
-        printf("46\n");
+        fprintf(stderr, "46\n");
         fclose(f);
     }
     return result;
@@ -57,7 +57,7 @@ int test_real_main_vs_expected(
     int res = __real_main(args_size, args);
     assert_int_equal(res, expected_code);
     if (res == 0) {
-        printf("56\n");
+        fprintf(stderr, "56\n");
         return cmp_file_and_str(expected_str);
     } else {
         return 0;
@@ -124,13 +124,13 @@ void run_sortings_with_different_correct_params(char* args[]) {
     assert_int_equal(0, 
         test_real_main_vs_expected(args, DEFAULT_ARGS_SIZE, SUCCESS_CODE, from_1_to_5));
     
-    printf("122\n");
+    fprintf(stderr, "122\n");
 
     args[2] = "5_string_inp_des.txt";
     assert_int_equal(0, 
         test_real_main_vs_expected(args, DEFAULT_ARGS_SIZE, SUCCESS_CODE, from_1_to_5));
     
-    printf("128\n");
+    fprintf(stderr, "128\n");
 
     // Run with descending comparator
     args[5] = "des";
