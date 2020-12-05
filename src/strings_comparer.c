5b1c0d08d77a4b371ab8a886fc5462ba855099f7
esvault <54893829+esvault@users.noreply.github.com>
2020-12-05 13:48:07 +0300

    Update and rename main.c to strings_comparer.c

--- src/strings_comparer.c
+++ src/strings_comparer.c
@@ -39,24 +39,24 @@ void sort(arguments_t arguments, strings_array_t strings) {
     }
 }
 
-//int alloc_strings(strings_array_t *array, long long strings_count) {
-//    (*array) = malloc(sizeof(char *) * strings_count);
-//    if ((*array) == NULL) {
-//        error("Cannot allocate array\n");
-//        return ERROR_EXIT_CODE;
-//    }
-//    for (long long i = 0; i < strings_count; ++i) {
-//        (*array)[i] = malloc(sizeof(char) * MAX_INPUT_STRING_SIZE);
-//        if ((*array)[i] == NULL) {
-//            for (long long j = 0; j < i; ++j) {
-//                free((*array)[j]);
-//            }
-//            error("Cannot allocate array\n");
-//            return ERROR_EXIT_CODE;
-//        }
-//    }
-//    return 0;
-//}
+int alloc_strings(strings_array_t *array, long long strings_count) {
+    (*array) = malloc(sizeof(char *) * strings_count);
+    if ((*array) == NULL) {
+        error("Cannot allocate array\n");
+        return ERROR_EXIT_CODE;
+    }
+    for (long long i = 0; i < strings_count; ++i) {
+        (*array)[i] = malloc(sizeof(char) * MAX_INPUT_STRING_SIZE);
+        if ((*array)[i] == NULL) {
+            for (long long j = 0; j < i; ++j) {
+                free((*array)[j]);
+            }
+            error("Cannot allocate array\n");
+            return ERROR_EXIT_CODE;
+        }
+    }
+    return 0;
+}
 
 void dealloc_strings(strings_array_t *array, array_size_t size) {
     for (array_size_t i = 0; i < size; ++i) {
@@ -66,34 +66,23 @@ void dealloc_strings(strings_array_t *array, array_size_t size) {
 }
 
 int read_strings_from_file(const char *filename, strings_array_t strings, long long strings_count) {
-    error("main.c 69\n");
     FILE *input = fopen(filename, "rt");
     if (input == NULL) {
         error("Cannot open file %s", filename);
         return ERROR_EXIT_CODE;
     }
-    error("main.c 75\n");
-    error("%lld\n", strings_count);
     for (int i = 0; i < strings_count; ++i) {
         if (fgets(strings[i], MAX_INPUT_STRING_SIZE, input) == NULL) {
             error("Error of reading from %s", filename);
             return ERROR_EXIT_CODE;
         }
-        error("%d: %s", i, strings[i]);
     }
-    error("main.c 82\n");
     char *last_string = strings[strings_count - 1];
-    error("%s --- %zu\n", last_string, strlen(last_string));
-    error("main.c 84\n");
     size_t last_string_len = strlen(last_string);
-    error("main.c 84\n");
     if (last_string[last_string_len - 1] != '\n') {
-        error("main.c 84\n");
         last_string[last_string_len] = '\n';
-        error("main.c 84\n");
         last_string[last_string_len + 1] = '\0';
     }
-    error("main.c 89\n");
     fclose(input);
     return 0;
 }
@@ -115,43 +104,25 @@ int write_string_to_file(const char *filename, strings_array_t strings, long lon
 }
 
 int main(int argc,  char *argv[]) {
-    error("%s\n", argv[1]);
     arguments_t arguments;
-    error("main.c 108\n");
     int result = set_parameters_values(argc, argv, &arguments);
     if (result != 0) {
         return result;
     }
-    error("main.c 109\n");
     strings_array_t strings = NULL;
-    strings = malloc(sizeof(char *) * arguments.strings_count);
-    if (strings == NULL) {
-        error("Cannot allocate array\n");
-        return ERROR_EXIT_CODE;
+    int allocation_result = alloc_strings(&strings, arguments.strings_count);
+    if (allocation_result != 0) {
+        return allocation_result;
     }
-    for (long long i = 0; i < arguments.strings_count; ++i) {
-        strings[i] = malloc(sizeof(char) * MAX_INPUT_STRING_SIZE);
-        if (strings[i] == NULL) {
-            for (long long j = 0; j < i; ++j) {
-                free(strings[j]);
-            }
-            error("Cannot allocate array\n");
-            return ERROR_EXIT_CODE;
-        }
-    }
-    error("main.c 111\n");
     int reading_result = read_strings_from_file(arguments.input_filename, strings, arguments.strings_count);
     if (reading_result != 0) {
         return reading_result;
     }
-    error("main.c 112\n");
     sort(arguments, strings);
-    error("main.c 113\n");
     int writing_result = write_string_to_file(arguments.output_filename, strings, arguments.strings_count);
     if (writing_result != 0) {
         return writing_result;
     }
-    error("main.c 114\n");
     dealloc_strings(&strings, arguments.strings_count);
     return 0;
 }
